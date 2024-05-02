f981bce7ae766bd02fbcfc789c56a3a6bc4b43c9
VadimDudorov <vadim090404@yandex.ru>
2024-05-03 01:49:20 +0300

    - Содание функционала расшифровки

--- src/Main.java
+++ src/Main.java
@@ -10,42 +10,48 @@ public class Main {
             'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ',
             'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з',
             'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ',
-            'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '\'', '\\', '\n', ':', '!', '?', ' '};
+            'ъ', 'ы', 'ь', 'э', 'ю', 'я', '.', ',', '«', '»', '"', '\'', '\\', '\n', ';', ':', '!', '?', ' ', '—'};
 
     public static void main(String[] args) throws IOException {
 
         Main main = new Main();
-        String path = main.validatePath();
-        List<Character> text = main.inputFile(path);
-        List<Character> encript = main.encryption(text);
-        main.outputFile(encript);
+
+//        String pathInput = main.validatePath();
+//        List<Character> textEncript = main.inputFile(pathInput);
+//        List<Character> encript = main.encryption(textEncript);
+//        main.outputFile(encript);
+
+        String pathOutput = main.validatePath();
+        List<Character> textDecoding = main.inputFile(pathOutput);
+        List<Character> decoding = main.decoding(textDecoding);
+        main.outputFile(decoding);
     }
 
     public String validatePath() {
         System.out.println("Введите путь к файлу (с расширением.txt) \nили нажмите Enter для дефолтного пути");
         Scanner scanner = new Scanner(System.in);
-            String text = scanner.nextLine();
-            if (text.isEmpty()) {
-                return "/Users/vadimdudorov/Downloads/in.txt";
-            } else {
-                char[] charsText = text.toCharArray();
-                if (charsText.length > 4) {
-                    int indexTxt = text.lastIndexOf(".");
-                    String substring = null;
-                    if (indexTxt != -1) {
-                        substring = text.substring(indexTxt);
-                    } else {
-                        throw new InputCryptanalysisException("Формат файла не равен .txt");
-                    }
-                    if (substring.equals(".txt")) {
-                        return text;
-                    } else {
-                        throw new InputCryptanalysisException("Формат файла не равен .txt");
-                    }
+        String text = scanner.nextLine();
+        if (text.isEmpty()) {
+            return "/Users/vadimdudorov/Downloads/inEncrypt.txt";
+        } else {
+            char[] charsText = text.toCharArray();
+            if (charsText.length > 4) {
+                int indexTxt = text.lastIndexOf(".");
+                String substring = null;
+                if (indexTxt != -1) {
+                    substring = text.substring(indexTxt);
                 } else {
-                    throw new InputCryptanalysisException("Не верный формат файла");
+                    throw new InputCryptanalysisException("Формат файла не равен .txt");
                 }
+                if (substring.equals(".txt")) {
+                    return text;
+                } else {
+                    throw new InputCryptanalysisException("Формат файла не равен .txt");
+                }
+            } else {
+                throw new InputCryptanalysisException("Не верный формат файла");
             }
+        }
     }
 
     public List<Character> inputFile(String path) throws IOException {
@@ -84,24 +90,27 @@ public class Main {
         }
     }
 
-    public List<Character> encryption(List<Character> text) {
+    public int displacement() {
         int displacement = 0;
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 System.out.println("Введите сдвиг");
                 if (scanner.hasNextInt()) {
                     displacement = scanner.nextInt();
-                    if (displacement > 0) {
-                        break;
+                    if (displacement > 0 && displacement < 76) {
+                        return displacement;
                     } else {
-                        System.out.println("Введите число больше 0");
+                        System.out.println("Введите число больше 0 и меньше 76");
                     }
                 } else {
                     System.out.println("Введите целое число");
                 }
             }
         }
+    }
 
+    public List<Character> encryption(List<Character> text) {
+        int displacement = displacement();
         List<Character> textEncryption = new ArrayList<>();
         for (Character iteratorText : text) {
             int index = 0;
@@ -114,6 +123,24 @@ public class Main {
             textEncryption.add(ALPHABET[index]);
         }
         return textEncryption;
+    }
+
+    public List<Character> decoding(List<Character> text) {
+        int displacement = displacement();
+        List<Character> decodingText = new ArrayList<>();
+        for (Character iterator : text) {
+            int index = 0;
+            for (int i = 0; i < ALPHABET.length; i++) {
+                if (iterator.equals(ALPHABET[i])) {
+                    index = Math.abs((i - displacement + ALPHABET.length) % ALPHABET.length);
+                    break;
+                } else {
+                    throw new InputCryptanalysisException("Переданного символа нет в алфавите " + iterator);
+                }
+            }
+            decodingText.add(ALPHABET[index]);
+        }
+        return decodingText;
     }
 
 }
